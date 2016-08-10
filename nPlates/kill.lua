e6d28983cdedb98a9c25d0580ac94492b54acc7d
Thomas Shannon <thomasjohnshannon@gmail.com>
2016-08-09 21:13:59 -0600

    Remove kill file.
    Removed kill file because Aggro Flash works.

--- nPlates/kill.lua
+++
@@ -1,9 +0,0 @@
--- Kill Unused Options
-
-for _, button in pairs({
-    'AggroFlash',
-}) do
-    _G['InterfaceOptionsNamesPanelUnitNameplates'..button]:SetAlpha(0)
-    _G['InterfaceOptionsNamesPanelUnitNameplates'..button]:SetScale(0.00001)
-    _G['InterfaceOptionsNamesPanelUnitNameplates'..button]:EnableMouse(false)
-end
