a1d564af4dd1bdcf80766375e250db45f53b85ca
Thomas Shannon <thomasjohnshannon@gmail.com>
2018-05-27 15:42:04 -0600

    8.0 First Push

--- core.lua
+++ core.lua
@@ -73,9 +73,9 @@ local function UpdateCastbar(frame)
 
     if ( frame.unit ) then
         if ( frame.castBar.casting ) then
-            notInterruptible = select(9, UnitCastingInfo(frame.displayedUnit))
+            notInterruptible = select(8, UnitCastingInfo(frame.displayedUnit))
         else
-            notInterruptible = select(8, UnitChannelInfo(frame.displayedUnit))
+            notInterruptible = select(7, UnitChannelInfo(frame.displayedUnit))
         end
 
         if ( UnitCanAttack("player", frame.displayedUnit) ) then
@@ -91,9 +91,16 @@ local function UpdateCastbar(frame)
 
         -- Force Icon Texture
 
-	if ( notInterruptible ) then
-		frame.castBar.Icon:SetTexture("Interface\\Icons\\Ability_Warrior_ShieldMastery")
-		frame.castBar.Icon:SetShown(true)
+	if ( notInterruptible or not frame.castBar.Icon:IsVisible() and frame.castBar.Background ) then
+		local _, class = UnitClass(frame.displayedUnit)
+		if ( class ) then
+			frame.castBar.Background:SetTexture("Interface\\Icons\\ClassIcon_"..class)
+		else
+			frame.castBar.Background:SetTexture("Interface\\Icons\\Ability_DualWield")
+		end
+		frame.castBar.Background:Show()
+	else
+		frame.castBar.Background:Hide()
 	end
 
         -- Abbreviate Long Spell Names
@@ -391,6 +398,15 @@ hooksecurefunc("DefaultCompactNamePlateFrameSetup", function(frame, options)
     if ( not frame.castBar.Icon.beautyBorder ) then
         nPlates:SetBorder(frame.castBar.Icon)
     end
+
+		-- Castbar Icon Background
+
+	if ( not frame.castBar.Background ) then
+		frame.castBar.Background = frame.castBar:CreateTexture("$parent_Background", "BACKGROUND")
+		frame.castBar.Background:SetAllPoints(frame.castBar.Icon)
+		frame.castBar.Background:SetTexture("Interface\\Icons\\Ability_DualWield")
+		frame.castBar.Background:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+	end
 
         -- Update Castbar
 
